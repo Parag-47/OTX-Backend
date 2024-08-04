974c229501aedc7c60773bcf09acc07d957c3367
Parag <ParagYadav74@gmail.com>
2024-08-04 19:21:13 +0530

    Updated

--- src/app.js
+++ src/app.js
@@ -2,6 +2,50 @@ import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
+import passport from "passport";
+import { Strategy as GoogleStrategy} from ("passport-google-oauth20");
+
+passport.use(new GoogleStrategy({
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/callback",
+  scope:["profile"]
+},
+function verify(issuer, profile, cb) {
+  db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
+    issuer,
+    profile.id
+  ], function(err, row) {
+    if (err) { return cb(err); }
+    if (!row) {
+      db.run('INSERT INTO users (name) VALUES (?)', [
+        profile.displayName
+      ], function(err) {
+        if (err) { return cb(err); }
+        
+        var id = this.lastID;
+        db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
+          id,
+          issuer,
+          profile.id
+        ], function(err) {
+          if (err) { return cb(err); }
+          var user = {
+            id: id,
+            name: profile.displayName
+          };
+          return cb(null, user);
+        });
+      });
+    } else {
+      db.get('SELECT * FROM users WHERE id = ?', [ row.user_id ], function(err, row) {
+        if (err) { return cb(err); }
+        if (!row) { return cb(null, false); }
+        return cb(null, row);
+      });
+    }
+  });
+}));
 
 const app = express();
 
@@ -11,6 +55,8 @@ app.use(
     origin: process.env.CORS_ORIGIN,
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(morgan("combined"));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
